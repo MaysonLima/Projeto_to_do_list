fc3b22bcfab81bcd2a6cd2442cb2c41567327c9e
MaysonLima <Maysonlima021@gmail.com>
2026-03-31 23:08:44 -0300

    anotações para proximas etapas

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*
+A fazer:
+
+[X] Botão funcionar
+[X] Pegar conteudo do input e criar li
+[X] Apagar conteudo
+[] Marcar como concluida
+[] Adicionar filtros
+[] Salvar no Local Storage
+
+Anotação para mim mesmo: É uma auto-referência curiosa fazer uma lista de tarefas dentro de uma lista de tarefas...
+
+
+*/
+
 function adicionarTarefa(){
     let input = document.querySelector("input").value
     let li = document.createElement("li")
